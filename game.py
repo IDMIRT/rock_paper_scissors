fe02ff1ee8850402dfc81a2be9107316792c6aa4
idmirt <ida78@inbox.ru>
2026-06-23 17:40:46 +1100

    Add scoring system

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 from random import choice
 import json
-import os
+import time
 
 selection_element = ['камень','ножницы','бумага']
 
@@ -8,8 +8,30 @@ selection_choice = {'камень':{'ножницы':True,'бумага':False},
                     'ножницы':{'бумага':True,'камень':False},
                     'бумага':{'камень':True,'ножницы':False}}  
 
-       
 
+class ScoreBoard:
+
+    def __init__(self, gamer = None):
+        self.file_scores = 'scores.json' 
+        if gamer == None:
+            self.gamer = 'Аноним'
+        else:
+            self.gamer = gamer
+        self.number_of_movies = 0
+        self.board = {self.gamer:0, 'Компьютер':0}
+    
+    def show_score(self):
+        return f'Результат игры {self.gamer} - {self.board[self.gamer]} компьютер - {self.board["Компьютер"]}'  
+
+    def add_point(self, winner):
+
+        self.board[winner] += 1 
+
+    def save_score(self):
+        with open(self.file_scores,'w',encoding='utf-8') as file:
+            json.dump(self.board,file,ensure_ascii=False)
+    
+        
 
 def game():
 
@@ -21,12 +43,13 @@ def game():
     
     gamer = input('Введите ваше имя: ')
     if gamer.strip == '':
-        gamer = 'Аноним'
+        gamer = "Аноним"
 
-    scores = {gamer:0, 'Компьютер':0}
+    scores = ScoreBoard(gamer)
 
     while True:
-        print(f'Результат игры {gamer} - {scores[gamer]} компьютер - {scores["Компьютер"]}')
+        print(scores.show_score())
+       
         print('Введите цифру выбора: \n' \
         '1 - камень \n' \
         '2 - ножницы\n' \
@@ -38,6 +61,7 @@ def game():
 
             if you_selection == '0':
                 print('Игра окончена')
+                scores.save_score()
                 break
             elif you_selection in ['1','2','3']:
                 key_comp_selection = choice(selection_element)
@@ -47,18 +71,20 @@ def game():
                     result = None
                 else:                 
                     result = selection_choice[key_you_selection][key_comp_selection]
-                    
+
+                time.sleep(0.5)    
                 if result == True:
-                    scores[gamer] += 1
+                    scores.add_point(gamer)
                     print(f'Выиграл {gamer}')
                 elif result == False:
-                    scores['Компьютер'] += 1
+                    scores.add_point('Компьютер')
                     print('Выиграл компьютер')
                 elif result == None:
                     print('Ничья')
-        
+            
         else:
             print(f'Неверный выбор значения {you_selection}')
        
+        print()
 
 game()
