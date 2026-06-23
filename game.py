daa3dc37c4db408e4fbea399016a13d31ceafb82
idmirt <ida78@inbox.ru>
2026-06-23 15:03:06 +1100

    Доработка логики убрал процедуру сравнения заменив на словарь

--- game.py
+++ game.py
@@ -1,26 +1,30 @@
 from random import choice
-selection_element = ['камень','ножицы','бумага']
-                  
 
-def selection_processing(user_selection, comp_selection):
-    """Процедура обработки выбора пользователя и игрока
-    user_selection - выбор пользоваетля
-    comp_selection - выбор компьютера
-    возврат True - выиграл пользователь False - выиграл компьютер, None - ничья"""
-    if user_selection == comp_selection:
-        return None
-    elif user_selection == 'камень' and comp_selection == 'ножницы':
-        return True
-    elif user_selection == 'ножницы' and comp_selection == 'камень':
-        return False
-    elif user_selection == 'ножницы' and comp_selection == 'бумага':
-        return True
-    elif user_selection == 'бумага' and comp_selection == 'ножницы':
-        return False
-    elif user_selection == 'бумага' and comp_selection == 'камень':
-        return True
-    elif user_selection == 'камень' and comp_selection == 'бумага':
-        return False
+selection_element = ['камень','ножницы','бумага']
+
+selection_choice = {'камень':{'ножницы':True,'бумага':False},
+                    'ножницы':{'бумага':True,'камень':False},
+                    'бумага':{'камень':True,'ножницы':False}}                 
+
+# def selection_processing(user_selection, comp_selection):
+#     """Процедура обработки выбора пользователя и игрока
+#     user_selection - выбор пользоваетля
+#     comp_selection - выбор компьютера
+#     возврат True - выиграл пользователь False - выиграл компьютер, None - ничья"""
+#     if user_selection == comp_selection:
+#         return None
+#     elif user_selection == 'камень' and comp_selection == 'ножницы':
+#         return True
+#     elif user_selection == 'ножницы' and comp_selection == 'камень':
+#         return False
+#     elif user_selection == 'ножницы' and comp_selection == 'бумага':
+#         return True
+#     elif user_selection == 'бумага' and comp_selection == 'ножницы':
+#         return False
+#     elif user_selection == 'бумага' and comp_selection == 'камень':
+#         return True
+#     elif user_selection == 'камень' and comp_selection == 'бумага':
+#         return False
 
 def game():
 
@@ -44,20 +48,33 @@ def game():
         '3 - бумага\n' \
         '0 - окончание игры')
         you_selection = input('Ваш выбор: ') 
-        if you_selection.strip in ['0','1','2','3']:
+        if you_selection.strip() in ['0','1','2','3']:
 
             if you_selection == '0':
                 print('Игра окончена')
                 break
             elif you_selection in ['1','2','3']:
-                comp_selection = choice(selection_element)
-                result = selection_processing(selection_element[int(you_selection)-1], comp_selection)
+                key_comp_selection = choice(selection_element)
+                key_you_selection = selection_element[int(you_selection)-1]
+                if key_you_selection == key_comp_selection:
+                    result = None
+                else:
+                    # result = selection_processing(selection_element[int(you_selection)-1], comp_selection)
+                    # key_selection = selection_element[int(key_you_selection)-1]
+                    # result = selection_choice[selection_element[int(you_selection)-1]][comp_selection]
+                    result = selection_choice[key_you_selection][key_comp_selection]
+
                 if result == True:
                     scores[gamer] += 1
-                else:
+                    print(f'Выиграл {gamer}')
+                elif result == False:
                     scores['Компьютер'] += 1
-
+                    print('Выиграл компьютер')
+                elif result == None:
+                    print('Ничья')
         
         else:
             print(f'Неверный выбор значения {you_selection}')
        
+
+game()
